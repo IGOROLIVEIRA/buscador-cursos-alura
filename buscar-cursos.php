b8ea4a25871d9f4bccc4d767cd338463efc3307f
Igor Afonso Oliveira Ruas <igorruas@Igors-MacBook-Pro.local>
2021-12-08 13:43:57 -0300

    initital project

---
+++ buscar-cursos.php
@@ -0,0 +1,19 @@
+<?php
+
+require 'vendor/autoload.php';
+require 'src/Buscador.php';
+
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
+
+$client = new Client(['base_uri' => 'https://www.alura.com.br/']);
+$crawler = new Crawler();
+
+$cursos = $crawler->filter('span.card-curso__nome');
+
+$buscador = new Buscador($client, $crawler);
+$cursos = $buscador->buscar('/cursos-online-programacao/php');
+
+foreach($cursos as $curso){
+    echo $curso . PHP_EOL;
+}
